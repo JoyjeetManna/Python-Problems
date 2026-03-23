e249980ec69d5ccd8a5bc5062a603f5e93151862
Joyjeet <joyjeetmanna2@gmail.com>
2026-03-23 20:25:09 +0530

    2nd try

---
+++ prime.py
@@ -0,0 +1,11 @@
+n=int(input("Enter n terms: "))
+count=1
+num=2
+while count!=n:
+    for i in range(2,num):
+        if(num%i==0):
+            break
+        else:
+            print(num,end=" ")
+            count=count+1
+        num=num+1   
